ac1ebd91f49e92a1f77c17a023ce8a589af866cf
f.romero@inizias.es <f.romero@inizias.es>
2020-02-26 12:57:05 +0100

    Agregar archivos de proyecto.

---
+++ MassTransit_Sample_Console/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GreenPipes;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Transactions;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using MassTransit.Util;
+
+namespace MassTransit_Sample_Console
+{
+    public class Message
+    {
+        public string Text { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host("rabbitmq://localhost");
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<Message>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync(); // This is important!
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Envie mensaje o pulse (q) para salir:");
+                var publicacion = Console.ReadLine();
+                if (publicacion.Equals("q"))
+                {
+                    break;
+                }
+                else
+                {
+                    await bus.Publish(new Message { Text = publicacion });
+                }
+            }
+            await bus.StopAsync();
+        }
+    }
+}
